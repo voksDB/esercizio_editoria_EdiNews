0e2145460480dcf5f0c37401f86c05fb03252850
Voks <dbianchi41w@gmail.com>
2025-07-06 00:04:42 +0200

    speriamo vada

--- ediNews/deploy.sh
+++ ediNews/deploy.sh
@@ -9,8 +9,8 @@ git init
 git add .
 git commit -m "Deploy Hugo site"
 git branch -M gh-pages
-git remote add origin https://github.com/voksDB/sito_hugo_editoria.git
+git remote add origin https://github.com/voksDB/esercizio_editoria_EdiNews.git
 git push -f origin gh-pages
 cd ..
 
-echo "✅ Sito pubblicato su: https://voksdb.github.io/sito_hugo_editoria/"
+echo "✅ Sito pubblicato su: https://voksdb.github.io/esercizio_editoria_EdiNews/"
